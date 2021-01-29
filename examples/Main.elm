cefad04bdc8282f38bf0ec21eca3a6eb9688be66
Kuria <012nkuria@gmail.com>
2021-01-29 10:12:54 +0300

    Update example to work with mustaches

--- examples/Main.elm
+++ examples/Main.elm
@@ -8,31 +8,29 @@ import Html.Attributes exposing (style)
 
 import Json.Print
 
-
 main =
-    textarea [ style [ ( "min-width", "40em" ), ( "min-height", "20em" ) ] ]
-        [ text <| Result.withDefault "" (Json.Print.prettyString 4 exampleJsonInput)
+    textarea [ style "min-width" "45em" , style "min-height" "24em"  ] 
+        [ text <| Result.withDefault "" (
+            Json.Print.prettyString { columns = 0, indent = 4, delimiter = '/' } exampleJsonInput
+            )
         ]
 
 
 exampleJsonInput =
     """
 {
-    "name": "Arnold",
-    "age": 70,
-    "isStrong": true,
-    "knownWeakness": null,
-    "nicknames": ["Terminator", "The Governator"],
-    "extra": {
-           "foo": "bar",
-           "zap": {
-                "cat": 1,
-                "dog": 2
-            },
-            "transport": [
-                [ "ford", "chevy" ],
-                [ "TGV", "bullet train", "steam" ]
-            ]
-    }
+  "name": "Arnold",
+  "age": 70,
+  "isStrong": true,
+  "knownWeakness": null,
+  "nicknames": ["Terminator", "The Governator"],
+  "extra": {
+    "foo": "bar",
+    "zap": { "cat": 1, "dog": 2 },
+    "transport": [
+      ["ford", "chevy"],
+      ["TGV", "bullet train", "steam"]
+    ]
+  }
 }
 """
